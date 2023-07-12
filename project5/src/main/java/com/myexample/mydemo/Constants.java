4417bc387fd34ad9b9083b851f446efba8bae549
Rosgardin <139356618+Rosgardin@users.noreply.github.com>
2023-07-12 17:29:40 +0400

    вф
    ф

--- project5/src/main/java/com/myexample/mydemo/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.myexample.mydemo;
-
-public class Constants {
-    public static final String API_SECRET_KEY = "mydemoapikey";
-    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
-}
